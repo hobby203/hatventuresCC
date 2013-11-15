3bfe440bc5b74b7b65892b3d72959fe8aea3a983
Edward Hobbs <edward24hobbs@outlook.com>
2013-11-15 20:31:08 +0000

    logger first attempt

--- Logger/Logger.lua
+++ Logger/Logger.lua
@@ -1,1 +1,31 @@
 rednet.open("back")
+
+function writeFile(message)
+  local logfile = io.open("disk/logs","a")
+  if logfile then
+    logfile.writeLine(message)
+    logfile:close()
+    return true
+  else
+    logfile:close()
+    return false
+  end
+end
+
+function receiveMessage()
+  id, message, distance = rednet.receive()
+  return message
+end
+
+function main()
+  log = receiveMessage()
+  if writeFile(log)
+    print("Log Added!")
+  else
+    print("Log failed, fix it")
+  end
+end
+
+while true do
+  main()
+end
