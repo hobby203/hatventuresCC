56da772b1aa1ac177e471ceb63df62f75292d683
Edward Hobbs <edward24hobbs@outlook.com>
2013-11-15 20:08:17 +0000

    fixed server auth problems, closing file handles and suchforth

--- Doors/Client.lua
+++ Doors/Client.lua
@@ -1,46 +1,46 @@
 rednet.open("bottom")
 
 function allowed(player,authServer)
-	rednet.send(authServer,player)
-	id, message, distance = rednet.receive()
-	return message
+  rednet.send(authServer,player)
+  id, message, distance = rednet.receive()
+  return message
 end
 
 function door(player,authServer)
-	if allowed(player,authServer) then
-		redstone.setOutput("top",true)
-		sleep(4)
-		redstone.setOutput("top",false)
-		return true
-	else
-		return false
-	end
+  if allowed(player,authServer) then
+    redstone.setOutput("top",true)
+    sleep(4)
+    redstone.setOutput("top",false)
+    return true
+  else
+    return false
+  end
 end
 
 function logger(player,logServer,state)
-	if state then
-		rednet.send(logServer,player.." was granted access "..os.time.." via computer "..os.getComputerID())
-	else
-		rednet.send(logServer,player.." tried to get in at "..os.time.." via computer "..os.getComputerID())
-	end
+  if state then
+    rednet.send(logServer,os.time().." "..player.." was granted access via computer "..os.getComputerID())
+  else
+    rednet.send(logServer,os.time().." "..player.." tried to get in at via computer "..os.getComputerID())
+  end
 end
 
 function getPlayer()
-	event, name = os.pullEvent("player")
-	return name
+  event, name = os.pullEvent("player")
+  return name
 end
 
 function main()
-	local authServer = 330
-	local logServer = 331
-	local player = getPlayer()
-	if door(player,authServer) then
-		logger(player,logServer,true)
-	else
-		logger(player,logServer,false)
-	end
+  local authServer = 330
+  local logServer = 331
+  local player = getPlayer()
+  if door(player,authServer) then
+    logger(player,logServer,true)
+  else
+    logger(player,logServer,false)
+  end
 end
 
 while true do
-	main()
+  main()
 end
--- Doors/Server.lua
+++ Doors/Server.lua
@@ -1,33 +1,39 @@
 rednet.open("bottom")
 
 function authorise(request)
-	players = fs.open("disk/allowed","r")
-	for player in players:lines() do
-		if request == player then
-			return true
-		end
-	end
-	return false
+  local players = io.open("disk/allowed","r")
+  if players then
+    for player in players:lines() do
+      if request == player then
+        players:close()
+        return true
+      end
+    end
+    players:close()
+    return false
+  else
+    error("File Not Found")
+  end
 end
 
 function receiveRequest()
-	id, request, distance = rednet.receive()
-	return id, request
+  id, request, distance = rednet.receive()
+  return id, request
 end
 
 function allowPlayer(door,state)
-	rednet.send(door,state)
+  rednet.send(door,state)
 end
 
 function main()
-	local id, player = receiveRequest()
-	if authorise(player) then
-		allowPlayer(id,true)
-	else
-		allowPlayer(id,false)
-	end
+  local id, player = receiveRequest()
+  if authorise(player) then
+    allowPlayer(id,true)
+  else
+    allowPlayer(id,false)
+  end
 end
 
 while true do
-	main()
+  main()
 end
